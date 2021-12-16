756fd912a71cd36d4ee10b75a9c0850a31f7f2d9
caitlin mcgraw <caitlinmcg10@gmail.com>
2021-12-16 12:24:28 -0700

    first exercise

---
+++ components/MainComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View } from 'react-native';
+import { CAMPSITES } from '../shared/campsites';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            selectedCampsite: null
+        };
+    }
+
+    onCampsiteSelect(campsiteId) {
+        this.setState({selectedCampsite: campsiteId});
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Directory
+                    campsites={this.state.campsites}
+                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
+                />
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(
+                        campsite => campsite.id === this.state.selectedCampsite)[0]}
+                />
+            </View>
+        );
+    }
+}
+
+export default Main;

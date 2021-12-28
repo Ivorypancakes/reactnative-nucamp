e6f0f5f0403706ad5ae336aff77aa20d1a86d3eb
caitlin mcgraw <caitlinmcg10@gmail.com>
2021-12-27 20:41:17 -0700

    week 2 working

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,24 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    };
+
+    newComment.date = new Date().toISOString();
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+
+};
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
